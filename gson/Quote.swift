2ba4b8d61f462f9ea2daca400dc86ab58c165daf
Olga Yushkevich <olyal-ya@mail.ru>
2020-11-16 12:28:21 +0300

    final

---
+++ gson/Quote.swift
@@ -0,0 +1,12 @@
+//
+//  quote.swift
+//  gson
+//
+//  Created by Mac on 15.11.2020.
+//
+
+   
+struct Quote: Decodable {
+    let content: String
+    let author: String
+}
--- gson/ViewController.swift
+++ gson/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var quoteLabel: UILabel!
+    @IBOutlet var authorLabel: UILabel!
+    
+    let URLAddress = "https://api.quotable.io/random"
+    
+    @IBAction func startButton() {
+        
+        guard let url = URL(string: URLAddress) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            guard let data = data else { return }
+            
+            do {
+                let quoteData = try JSONDecoder().decode(Quote.self, from: data)
+                DispatchQueue.main.async {
+                    self.quoteLabel.text = "\(quoteData.content)"
+                    self.authorLabel.text = quoteData.author
+                }
+            } catch let error {
+                print(error)
+            }
+        }.resume()
     }
-
-
+    
 }
-
